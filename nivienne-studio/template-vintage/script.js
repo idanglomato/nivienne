529456417b6b8bdabc611f95b44f54614263e0fa
Tommy Anglomas <84143407+idanglomato@users.noreply.github.com>
2026-06-07 22:33:35 +0700

    Add files via upload

--- nivienne-studio/template-vintage/script.js
+++ nivienne-studio/template-vintage/script.js
@@ -49,9 +49,9 @@
   ];
 
   var DURATION  = 3600;   // total loader duration ms
-  var TICK_RATE = 100;     // ms between % updates
-  var DELAY_MAX = 100;     // constant 100ms
-  var DELAY_MIN = 100;     // constant 100ms
+  var TICK_RATE = 200;     // ms between % updates
+  var DELAY_MAX = 200;     // constant 100ms
+  var DELAY_MIN = 200;     // constant 100ms
 
   var loaderEl = document.getElementById('loader-screen');
   var fill     = document.getElementById('loader-bar-fill');
